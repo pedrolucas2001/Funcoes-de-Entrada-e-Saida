f19dc76fe8007383fc78b3e9672689f8b9cf2615
Pedro Lucas <108367468+pedrolucas2001@users.noreply.github.com>
2022-11-21 23:43:58 -0300

    Add files via upload
    IDE - CODE BLOCKS

---
+++ exercicioLerDados/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero;
+
+    printf("Digite um numero: ");
+    scanf("%d",&numero);
+
+    if (numero >0)
+        printf("O numero é POSITIVO!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
